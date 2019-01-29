19d3c3500ae636eecf5fc9f39e88cdc15ed7f87c
ayho <y.houssin01@gmail.com>
2019-01-29 21:50:16 +0100

    adding links

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div>
     <section class="welcome">
-      <h1>::Here is my <span>playground</span>::</h1>
+      <h1 class="welcome-title">::Here is my <span>playground</span>::</h1>
+      <ul class="welcome-nav">
+
+        <li>
+          <nuxt-link to="/events">Event Animation</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/asia">Asia Hero</nuxt-link>
+        </li>
+        <li>
+          <nuxt-link to="/slice">Slice Appearing</nuxt-link>
+        </li>
+      </ul>
     </section>
   </div>
 </template>
@@ -16,7 +28,29 @@
     align-items: center;
     justify-content: center;
     height: 100vh;
+    flex-direction: column;
+    &-title {
+      display: block;
+    }
+    &-nav {
+      padding: 0;
+      list-style: none;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      margin-top: 2rem;
+      li {
+        margin-bottom: 1.5rem;
+      }
+      a {
+        font-size: 1.2rem;
+        font-family: "Source Code Pro", sans-serif;
+        color: #47494e;
+      }
+    }
   }
+
   h1 {
     font-size: 4rem;
     font-weight: lighter;
@@ -24,4 +58,5 @@
       font-weight: bold;
     }
   }
+
 </style>
